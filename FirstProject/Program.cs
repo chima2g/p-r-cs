92e5b8c72afd63c2f8c4465a5b51ba8c34c300f8
chima2g <chima2g@hotmail.com>
2019-07-22 17:07:31 +0100

    added convertCSVStrToArr

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace FirstProject
 {
@@ -11,7 +12,31 @@ namespace FirstProject
         int BONUS_TYPE_1 = 1;
         int BONUS_TYPE_2 = 2;
 
-        private int getBonus (string caseValue, decimal threshold, decimal target) {
+        //The amount a case has to be above before a broker can begin to receive bonus for each bonus structure
+        int THRESHOLD_AMOUNT_1 = 100000;
+        int THRESHOLD_AMOUNT_2 = 250000;
+
+        //The target brokers have to hit in order to receive bonus for each bonus structure
+        int TARGET_AMOUNT_1 = 10000;
+        int TARGET_AMOUNT_2 = 50000;
+
+        const CURRENCY_LOOKUP = { $: 0.8 }; //Lookup object for converting foreign currencies into GBP
+
+        private ArrayList convertCSVStrToArr (string csvStr) {
+            string[] lines = csvStr.Split("\r\n"); //Get each line of the CSV string
+            ArrayList dataArr = new ArrayList();
+
+            //Convert each line to an array of values and add the array to the array
+            foreach (string line in lines)
+            {
+                dataArr.Add(line.Split(","));
+            }
+
+            return dataArr;
+        }
+
+
+        private int bonusCalculator (string caseValue, decimal threshold, decimal target) {
             decimal caseValueAsDec = decimal.Parse(caseValue.Substring(1)); //Convert caseValue into float format
             int totalBonus = 0;
 
